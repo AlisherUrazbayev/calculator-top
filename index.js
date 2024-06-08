2887b0c92197d7ab08b36521e87cd632f4711984
Alisher Urazbayev <Alisher1518204240@gmail.com>
2024-06-08 10:16:33 -0400

    Write function to make math operator choice
    The function is created to invoke diffrent math function
    depending on operand provided.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-checkConsoleResult();
+let firstNumber;
+let secondNumber;
+let operator;
 
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+  }
+}
 function checkConsoleResult() {
   let result = 0;
 
